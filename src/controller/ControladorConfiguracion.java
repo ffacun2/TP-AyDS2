66181cd02c94ff7a56bee7394e3dbcc20cf249d6
Facundo Criado <facu-1999-44@hotmail.com>
2025-04-03 10:56:56 -0300

    No se habia subido xd

--- src/controller/ControladorConfiguracion.java
+++ src/controller/ControladorConfiguracion.java
@@ -7,12 +7,39 @@ import model.Usuario;
 
 public class ControladorConfiguracion implements ActionListener{
 
-	private VentanaConfiguracion ventanaPrincipal;
-	private Contacto contactoActual; // representa al contacto con el que se esta escribiendo
+	private VentanaConfiguracion ventanaConfiguracion;
+	private ControladorPrincipal controladorPrincipal;
+	
+	public ControladorConfiguracion() {
+		this.ventanaConfiguracion = new VentanaConfiguracion("Sistema de Mensajeria Instantanea");
+		this.ventanaConfiguracion.setLocationRelativeTo(null);
+		this.ventanaConfiguracion.setVisible(true);
+		this.controladorPrincipal = new ControladorPrincipal();
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String comando = e.getActionCommand();
 		
+		if (comando.equals("INGRESAR")) {
+			
+			// Si el metodo crear usuario devuelve true, se cierra la ventana de configuracion y se abre la ventana principal
+			// Caso contrario se muestra un mensaje de error.
+			if(controladorPrincipal.crearUsuario(
+					this.ventanaConfiguracion.getIp(), 
+					this.ventanaConfiguracion.getPuerto(), 
+					this.ventanaConfiguracion.getNickname())) {
+				this.ventanaConfiguracion.dispose();
+				this.controladorPrincipal.mostrarVentanaPrincipal();
+			}
+			else {
+				ventanaConfiguracion.mostrarError("Error al crear el usuario");
+			}
+		}
 	}
+	
+	private void mostratError(String mensaje) {
+		JOptionPane.showMessageDialog(ventanaConfiguracion, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+	}
+
 }
--- src/controller/ControladorPrincipal.java
+++ src/controller/ControladorPrincipal.java
@@ -1,5 +1,76 @@
 package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControladorPrincipal {
+import model.Contacto;
+import model.Conversacion;
 
-}
+
+public class ControladorPrincipal implements ActionListener {
+	
+	private VentanaPrincipal ventanaPrincipal;
+	private Usuario usuario;
+	private Contacto contactoActivo; //representa el contacto que tiene el chat abierto
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String comando = e.getActionCommand();
+		
+		if (comando.equals("CREAR_CONTACTO")) {
+			
+		}
+		else if (comando.equals("CREAR_CONVERSACION")) {
+			
+		}
+		else if ( comando.equals("ENVIAR_MENSAJE")) {
+			
+		}
+		
+	}
+	
+	/**
+	 *  Crea un nuevo usuario en la aplicacion, si este se crea correctamente se cierra
+	 *  la ventana de configuracion y se abre la ventana principal.
+	 *  Caso contrario se muestra un mensaje de error.
+	 *  Quien determine si el usuario se crea correctamente es el cliente usuario que lanza error de socket.
+	 *  
+	 *  @param ip - ip del usuario
+	 *  @param puerto - puerto del usuario
+	 *  @param nickname - nombre del usuario
+	 */
+	public boolean crearUsuario(String ip, int puerto, String nickname) {
+		
+	}
+
+	
+	/**
+	 * Obtiene la conversacion que se ha seleccionado para abrir el chat
+	 * 
+	 * @return Conversacion - devuelve la conversacion del contacto seleccionado.
+	 */
+	public Conversacion onSeleccionarConversacion() {	
+	}
+	
+	/**
+	 * Crea un nuevo contacto en la lista de contactos
+	 * 
+	 * @param ip - ip del contacto
+	 * @param puerto - numero del contacto
+	 * @param nickname - nombre del contacto
+	 */
+	public void crearContacto(String ip, int puerto, String nickname) {
+	}
+	
+	/**
+	 * 
+	 */
+ 	public void crearConversacion(Contacto contacto) {
+ 	}
+ 	
+ 	
+ 	public void mostrarVentanaPrincipal() {
+ 		this.ventanaPrincipal = new VentanaPrincipal("Sistema de Mensajeria Instantanea");
+ 		this.ventanaPrincipal.setLocationRelativeTo(null);
+ 		this.ventanaPrincipal.setVisible(true);
+ 	}
