51d360b55d5ccd0b84c7f00d82d7b09da529296e
Guido-Bailon <guidobailon@gmail.com>
2025-04-04 18:16:18 -0300

    Ventana principal

--- src/view/VentanaPrincipal.java
+++ src/view/VentanaPrincipal.java
@@ -1,38 +1,109 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class VentanaPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textFieldMensaje;
 
 	/**
 	 * Launch the application.
 	 */
-	/*
-	 * public static void main(String[] args) { EventQueue.invokeLater(new
-	 * Runnable() { public void run() { try { VentanaPrincipal frame = new
-	 * VentanaPrincipal("Sistema de mensajeria instantanea");
-	 * frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); } } });
-	 * }
-	 */
+	
+	 public static void main(String[] args) { EventQueue.invokeLater(new
+	  Runnable() { public void run() { try { VentanaPrincipal frame = new
+	  VentanaPrincipal("Sistema de mensajeria instantanea");
+	  frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); } } });
+	  }
 
 	/**
 	 * Create the frame.
 	 */
 	public VentanaPrincipal(String title) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 503, 312);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		this.setTitle(title);
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane, BorderLayout.CENTER);
+		splitPane.setMinimumSize(new Dimension(100,100));
+		
+		JPanel panel = new JPanel();
+		splitPane.setRightComponent(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelCuerpo = new JPanel();
+		panel.add(panelCuerpo, BorderLayout.SOUTH);
+		panelCuerpo.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelMensaje = new JPanel();
+		panelCuerpo.add(panelMensaje, BorderLayout.CENTER);
+		panelMensaje.setLayout(new BorderLayout(0, 0));
+		
+		textFieldMensaje = new JTextField();
+		panelMensaje.add(textFieldMensaje, BorderLayout.NORTH);
+		textFieldMensaje.setColumns(10);
+		
+		JPanel panelBtnEnviar = new JPanel();
+		panelCuerpo.add(panelBtnEnviar, BorderLayout.EAST);
+		panelBtnEnviar.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnEnviar = new JButton("Enviar");
+		panelBtnEnviar.add(btnEnviar, BorderLayout.NORTH);
+		
+		JScrollPane scrollPaneConv = new JScrollPane();
+		panel.add(scrollPaneConv, BorderLayout.CENTER);
+		
+		JTextArea textAreaConv = new JTextArea();
+		scrollPaneConv.setViewportView(textAreaConv);
+		
+		JPanel panelIzq = new JPanel();
+		splitPane.setLeftComponent(panelIzq);
+		panelIzq.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panelIzq.add(scrollPane, BorderLayout.CENTER);
+		
+		JPanel panelOpt = new JPanel();
+		panelIzq.add(panelOpt, BorderLayout.NORTH);
+		panelOpt.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JPanel panelAgrCont = new JPanel();
+		panelOpt.add(panelAgrCont);
+		panelAgrCont.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnAgrContacto = new JButton("Agregar contacto");
+		panelAgrCont.add(btnAgrContacto, BorderLayout.CENTER);
+		
+		JPanel panelNueConv = new JPanel();
+		panelOpt.add(panelNueConv);
+		panelNueConv.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNueConv = new JButton("Nueva conversacion");
+		panelNueConv.add(btnNueConv, BorderLayout.CENTER);
+	}
+
+	public String getMensaje() {
+		return this.textFieldMensaje.getText();
 	}
 
 }
